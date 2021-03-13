7c96530bdebcedf7fd6ae7d9d3f1d4c101d488a3
MeeraMeera1 <gatesameera98@gmail.com>
2021-03-13 18:18:12 -0500

    set up helper for responsiveness

---
+++ react-app/src/components/HomePage/FirstPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { useMediaQuery } from "react-responsive";
+import styled from "styled-components";
+import { deviceSize } from "../responsive";
+
+import FriendGroup from "../../assets2/friendgroup.svg";
+import FirstPageText from "../../assets2/firspagtxt.svg";
+
+export function FirstPage(props) {
+    const { children } = props;
+}
--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -1,3 +1,10 @@
 import React from "react";
 import styled from "styled-components";
+import { PageContainer } from "../pageContainer";
 
+export function HomePage(props) {
+    return (
+        <PageContainer>
+        </PageContainer>
+    )
+}
