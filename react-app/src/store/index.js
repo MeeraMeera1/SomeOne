fa9a9799596bd9564edd261b7a2e0a816d4148e8
MeeraMeera1 <gatesameera98@gmail.com>
2021-03-15 00:50:06 -0400

    initialized store session and modal

---
+++ react-app/src/store/index.js
@@ -0,0 +1,25 @@
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+
+import session from "./session";
+import Modal from "./modal";
+
+const rootReducer = combineReducers({
+  session,
+  Modal,
+});
+
+let enhancer;
+
+if (process.env.NODE_ENV === "production") {
+  enhancer = applyMiddleware(thunk);
+} else {
+  const logger = require("redux-logger").default;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+}
+
+export default function configureStore(preloadedState) {
+  return createStore(rootReducer, preloadedState, enhancer);
+}
---
+++ react-app/src/store/session.js
@@ -0,0 +1,68 @@
+const USER = "sesssion/USER";
+
+const setSession = (user) => ({ type: USER, user });
+
+export const authenticate = () => async (dispatch) => {
+  const response = await window.fetch("/api/auth/", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const user = await response.json();
+  if (user.errors) return dispatch(setSession(null));
+  dispatch(setSession(user));
+};
+
+export const login = (email, password) => async (dispatch) => {
+  const response = await window.fetch("/api/auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  });
+  const user = await response.json();
+  if (user.errors) {
+    dispatch(setSession(null));
+    return user.errors;
+  }
+  dispatch(setSession(user));
+};
+
+export const signUp = (username, email, password) => async (dispatch) => {
+  const response = await window.fetch("/api/auth/signup", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username,
+      email,
+      password,
+    }),
+  });
+  const user = await response.json();
+  if (user.errors) {
+    dispatch(setSession(null));
+    return user.errors;
+  }
+  dispatch(setSession(user));
+};
+
+export const logout = () => async (dispatch) => {
+  await window.fetch("/api/auth/logout", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  dispatch(setSession(null));
+};
+
+export default function reducer(state = { user: null, loaded: false }, action) {
+  switch (action.type) {
+    case USER:
+      return { ...state, user: action.user, loaded: true };
+    default:
+      return state;
+  }
+}
