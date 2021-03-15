89f1d3aa1a23719d54ad9e740d8a2d19e58a3df5
MeeraMeera1 <gatesameera98@gmail.com>
2021-03-15 04:41:18 -0400

    first page styled as good as i could get

--- react-app/src/components/HomePage/FirstPage.js
+++ react-app/src/components/HomePage/FirstPage.js
@@ -7,8 +7,9 @@ import FriendGroup from "../../assets2/friendgroup.svg";
 import FirstPageText from "../../assets2/firspagtxt.svg";
 
 const PageContainer = styled.div`
+  position: relative;
   width: 100%;
-  height: 900px;
+  height: 100vh;
   background-color: #e4e5c3;
   background-position: 0px -150px;
   background-size: cover;
@@ -16,28 +17,28 @@ const PageContainer = styled.div`
 `;
 
 const FirstPageTextCon = styled.div`
-    width: 90em;
+    width: 100%;
     height: 15em;
     position: relative;
     top: 80px;
 
     img {
+      width: 100%;
       position: absolute;
       bottom:25px;
     }
 `;
 
-const FriendGroupCon = styled.div `
-  width: 99em;
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  
-  img {
-    width: 99em;
-  }
-  
+const FriendGroupCon = styled.div`
+  width: 100%;
+  height: 100%;
 
+  img {
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+  }
 `;
 
 export function FirstPage(props) {
