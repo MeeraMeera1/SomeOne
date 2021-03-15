03b69ec6b53ea6082ab6636c32d11b9ed3e1fca2
MeeraMeera1 <gatesameera98@gmail.com>
2021-03-15 02:00:59 -0400

    getUsers thunk

--- react-app/src/store/user.js
+++ react-app/src/store/user.js
@@ -1,5 +1,6 @@
 const SET_USER = "user/USER";
-const CREATE_USER = "user/CREATEUSER";
+const SET_USERS = "user/USERS"
+
 
 const setUser = (user) => {
     return {
@@ -8,14 +9,30 @@ const setUser = (user) => {
     }
 }
 
-const createUser = (user) => {
+const setUsers = (users) => {
     return {
-        type: CREATE_USER,
-        user
+        type: SET_USERS,
+        users
     }
 }
 
-export const getUsers = () => async(dispatch) => {
-    const response = await fetch('/api/users/');
-    if 
+export const getUser = () => async(dispatch) => {
+    const response = await fetch(`/api/users/${userId}`);
+    if (response.ok) {
+        const user = await response.json()
+        dispatch(setUser(user))
+        return user;
+    }
 }
+
+export const getUsers = () => async (dispatch) => {
+    const response = await fetch("api/users");
+    if (response.ok) {
+        const users = await response.json()
+        dispatch(setUsers(users))
+        return users;
+    }
+}
+
+
+export const getUsers 
