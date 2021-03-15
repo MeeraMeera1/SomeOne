2fd6d11f2cb8ddc374d945d70e8db9e1182a641e
MeeraMeera1 <gatesameera98@gmail.com>
2021-03-14 21:10:04 -0400

    create post route created

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -17,3 +17,26 @@ def get_posts():
     else:
         return {'posts': []}
 
+@post_routes.route('/', methods=['POST'])
+def create_post():
+    form = CreatePostForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        post = Post()
+        form.populate_obj(post)
+        db.session.add(post)
+        db.session.commit()
+
+        if 'images' in request.files:
+            images = request.files.getlist('images')
+            for image in images:
+                if allowed_file(image.filename):
+                    image.filename = get_unique_filename(image.filename)
+                    image_url = upload_file_to_s3(image, Config.S3_BUCKET)
+                    image = Post(post_id=post.id, image_url=imgUrl)
+                    db.session.add(image)
+            db.session.commit()
+        return post.to_dict()
+
+
+
