b058c3a9276bcb917e9ab2fcc6d9f1287d423604
MeeraMeera1 <gatesameera98@gmail.com>
2021-03-19 02:18:31 -0400

    made changes to forms

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -9,6 +9,22 @@ const Background = styled.img`
   z-index: -1;
 `;
 
+const ButtonContainer = styled.div`
+  display: block;
+`;
+
+const CircleButton = styled.button`
+  border: none;
+  outline: none;
+  background-color: white;
+  cursor: pointer;
+  transition: all 200ms ease-in-out;
+
+  &:hover {
+    background-color: black;
+  }
+`;
+
 const HomePage = () => {
   const bgs = [
     "https://someone01.s3.amazonaws.com/firstpage.png",
