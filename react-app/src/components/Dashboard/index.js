de8931fed9201d0b1e13c3b18cd994f8312129da
MeeraMeera1 <gatesameera98@gmail.com>
2021-03-23 17:21:56 -0400

    dash sections created

--- react-app/src/components/Dashboard/index.js
+++ react-app/src/components/Dashboard/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Logo from "../../assets2/logo.svg"
 import "./dash.css";
 
 
@@ -8,11 +9,31 @@ const DashBoard = () => {
   return (
     <>
       <div className="container">
-        <div className="side-nav"></div>
-        <div className="main"></div>
+        <div className="side-nav">
+          <div className="side-menu">
+            <div className="logo">
+              <img src={Logo} alt="someOne" />
+            </div>
+            <ul>
+              <li>
+                <a href="/dash">
+                  <span>DashBoard</span>
+                </a>
+              </li>
+              <li>
+                <a href="/chatroom">
+                  <span>Chat Party</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="main">
+          <h2>Users DashBoard</h2>
+          <button className="create-post" onClick={createPostForm}>Create Post</button>
+        </div>
         <div className="profile-sec"></div>
       </div>
-
     </>
   );
 };
