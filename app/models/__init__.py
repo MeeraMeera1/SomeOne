11dc205f60ab5d53b3f873bcb94a8e3f16c10e67
MeeraMeera1 <gatesameera98@gmail.com>
2021-03-25 01:23:33 -0400

    chat room model created

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,4 +4,5 @@ from .display_name import DisplayName
 from .post import Post
 from .post_like import PostLike 
 from .comment import Comment 
-from .comment_like import CommentLike 
+from .comment_like import CommentLike
+from .chat_room import ChatRoom
--- react-app/src/components/Navbar/index.js
+++ react-app/src/components/Navbar/index.js
@@ -13,47 +13,47 @@ import LogoutButton from "../auth/LogoutButton";
 import Logo from "../../assets2/logo.svg"
 
 const NavBar = () => {
-  const sessionUser = useSelector((state) => state.session.user);
-  const width = useWindowWidth();
+  // const sessionUser = useSelector((state) => state.session.user);
+  // const width = useWindowWidth();
 
-  let sessionLinks;
+  // let sessionLinks;
 
-  if (!!sessionUser) {
-    if (width > 800) {
-      sessionLinks = (
-        <>
-          <LogoutButton />
-        </>
-      );
-    } else {
-      sessionLinks = null;
-    }
-  } else {
-    if (width > 800) {
-      sessionLinks = (
-        <>
-          {/* <li className="nav__item">
-            <LoginFormModal />
-          </li>
-          <li className="nav__item">
-            <SignUpFormModal />
-          </li> */}
-          {/* <li className="nav__item">
-            <NavLink
-              className="nav__item"
-              to="/browse"
-              exact={true}
-              activeClassName="active"
-            >
-              Get Started
-            </NavLink>
-          </li>
-        </>
-      );
-    } else {
-      sessionLinks = <SessionLinksDropdown />;
-    } */}
-  }
+  // if (!!sessionUser) {
+  //   if (width > 800) {
+  //     sessionLinks = (
+  //       <>
+  //         <LogoutButton />
+  //       </>
+  //     );
+  //   } else {
+  //     sessionLinks = null;
+  //   }
+  // } else {
+  //   if (width > 800) {
+  //     sessionLinks = (
+  //       <>
+  //         {/* <li className="nav__item">
+  //           <LoginFormModal />
+  //         </li>
+  //         <li className="nav__item">
+  //           <SignUpFormModal />
+  //         </li> */}
+  //         {/* <li className="nav__item">
+  //           <NavLink
+  //             className="nav__item"
+  //             to="/browse"
+  //             exact={true}
+  //             activeClassName="active"
+  //           >
+  //             Get Started
+  //           </NavLink>
+  //         </li>
+  //       </>
+  //     );
+  //   } else {
+  //     sessionLinks = <SessionLinksDropdown />;
+  //   } */}
+  // }
 
   return (
     <header className="sticky z-10 w-full top-0 bg-transparent">
