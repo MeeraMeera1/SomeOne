886777b99e4566a2215a5c18445a6990cc845fff
MeeraMeera1 <gatesameera98@gmail.com>
2021-03-25 01:27:29 -0400

    displayname and chatroom form initialized

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -2,3 +2,5 @@ from .login_form import LoginForm
 from .signup_form import SignUpForm
 from .createpost_form import CreatePostForm
 from .createcomment_form import CreateCommentForm
+from .display_name_form import DisplayNameForm
+from .chat_room_form import ChatRoomForm 
