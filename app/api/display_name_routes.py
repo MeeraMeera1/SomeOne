cadc1161141b6c782bac045fa69ee0a158d87fc1
MeeraMeera1 <gatesameera98@gmail.com>
2021-03-25 02:05:04 -0400

    post route for displaynames created

--- app/api/display_name_routes.py
+++ app/api/display_name_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify
 from app.models import db, DisplayName
 from app.forms import DisplayNameForm
+from app.api.auth_routes import validation_errors_to_error_messages
 
 display_name_routes = Blueprint("displaynames", __name__)
 
@@ -11,4 +12,26 @@ def get_displaynames():
     """
     display_names = DisplayName.query.all()
     return {"displaynames": [display_name.to_dict() for display_name in display_names]}
-    
+
+@display_name_routes.route("", methods=["POST"])
+def create_display_name():
+    """
+    Create display name
+    """
+    form = DisplayNameForm()
+    form["csrf_token"].data = request.cookies["csrf_token"]
+
+    if form.validate_on_submit():
+        new_display_name = DisplayName(
+            display_name = form.data["display_name"], 
+        )
+        db.session.add(new_display_name)
+        db.session.commit()
+        return new_display_name.to_dict()
+
+    errors = validation_errors_to_error_messages(form.errors)
+    return {"errors": errors}
+
+
+
+
