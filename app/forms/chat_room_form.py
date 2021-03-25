5c4ece9d4f49eb4fb6121744ca3654c9d835d9e6
MeeraMeera1 <gatesameera98@gmail.com>
2021-03-25 01:44:41 -0400

    chat room flask form

--- app/forms/chat_room_form.py
+++ app/forms/chat_room_form.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm 
-from wtforms import StringField
+from wtforms import StringField, TextAreaField, IntegerField
 from wtforms.validators import DataRequired 
 
 class ChatRoomForm(FlaskForm):
     room_name = StringField('room_name', validators=[DataRequired()])
+    room_topic = TextAreaField('room_topic', validators=[DataRequired()])
+    display_name_id = IntegerField("displayname", validators=[DataRequired()])
